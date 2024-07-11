aaf41372f528e25d2d9b4cef9e1006c074a62ad7
pakanwitk <pakarwit@gmail.com>
2024-07-11 23:36:47 +0700

    Update next config

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  assetPrefix: process.env.BASE_URL
-    ? `https://${process.env.BASE_URL}`
-    : undefined,
-};
-
-export default nextConfig;
